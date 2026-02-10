cdb10d933df8cad0b4be0edd9f77bec10230f682
DAVID JIMENEZ <davidinformatica18@gmail.com>
2026-02-10 12:40:30 +0100

    Update Test_api.py

--- Prueba_Pokemon/Test/Test_api.py
+++ Prueba_Pokemon/Test/Test_api.py
@@ -1,20 +1,25 @@
 from fastapi import FastAPI
+from pathlib import Path
 import json
 
 app = FastAPI()
 
+# Ruta del JSON
 RUTA_JSON = Path(__file__).parent / "lista_pokemon.json"
 
-with open("lista_pokemon.json", "r", encoding="utf-8") as archivo:
+# Archivo el JSON
+with open(RUTA_JSON, "r") as archivo:
     pokemons = json.load(archivo)
 
-@app.get("/")
-def lista_pokemon(): # Lista de pokmovimientos.
+@app.get("/pokemon") # Lista de Pokémon
+def lista_pokemon():
     return pokemons
 
-@app.get("/")
-def pokemovimientos (): # Lista de pokmovimientos.
-     for pokemon in pokemons:
+@app.get("/movimientos") # Lista de movimientos
+def pokemovimientos():
+    movimientos = []
+
+    for pokemon in pokemons:
         for movimiento in pokemon["movimientos"]:
             movimientos.append({
                 "pokemon": pokemon["nombre"],
@@ -22,18 +27,23 @@ def pokemovimientos (): # Lista de pokmovimientos.
                 "tipo": movimiento["tipo"],
                 "poder": movimiento["poder"]
             })
-return movimientos
 
-@app.post("/") 
-def crear ():
-    return
-@app.put("/items/{item_id}")
-def actualizar():
-    return
-@app.patch("/items/{item_id}")
-def actualizar_parte():
-    return
+    return movimientos
 
-#@app.delete("/items/{item_id}")
-    #def eliminar():
-        #return
+
+@app.post("/pokemon")
+def crear():
+    return {"mensaje": "Se ha creado el Pokémon"}
+
+@app.put("/pokemon/{item_id}")
+def actualizar(item_id: int):
+    return {"mensaje": f"Se ha actualizado el Pokémon {item_id}"}
+
+
+@app.patch("/pokemon/{item_id}")
+def actualizar_parte(item_id: int):
+    return {"mensaje": f"Se ha actualizado parte del Pokémon {item_id}"}
+
+@app.delete("/pokemon/{item_id}")
+def eliminar(item_id: int):
+    return {"mensaje": f"Se ha eliminado el Pokémon {item_id}"}
